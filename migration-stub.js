65a1d3a5320048231d864abd9e6920209832105b
JasonPaulino <jasonp0830@gmail.com>
2023-04-28 18:23:06 -0400

    added rest of the files needed

---
+++ migration-stub.js
@@ -0,0 +1,11 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => {};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => {};
