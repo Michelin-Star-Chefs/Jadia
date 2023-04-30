69f978c78d048fc90d1188325da0429de1cf08c6
duracellB <i.schlae.h@gmail.com>
2023-04-30 14:07:20 -0400

    minor edits

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -1,17 +1,34 @@
 const knex = require("../knex");
 
 class Post {
+  constructor(
+    post_id,
+    user_id,
+    location,
+    start_date,
+    end_date,
+    photo,
+    description
+  ) {
+    this.post_id = post_id;
+    this.user_id = user_id;
+    this.location = location;
+    this.start_date = start_date;
+    this.end_date = end_date;
+    this.photo = photo;
+    this.description = description;
+  }
   //create
-  // static async create() {
-  //   try {
-  //     const postInsert = await knex.raw()
-  //     const newPost = postInsert.rows[0]
-  //     return newPost
-  //   } catch (err) {
-  //     console.log(err)
-  //     return null
-  //   }
-  // }
+  static async create() {
+    try {
+      const postInsert = await knex.raw()
+      const newPost = postInsert.rows[0]
+      return newPost
+    } catch (err) {
+      console.log(err)
+      return null
+    }
+  }
   //list
   //update
   //delete
