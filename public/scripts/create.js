863c2331fe3cf8c9911e0d41f572f70392956842
JasonPaulino <jasonp0830@gmail.com>
2023-05-07 14:19:35 -0400

    completed the login and signup with some validation

--- public/scripts/create.js
+++ public/scripts/create.js
@@ -1,6 +1,36 @@
 /* eslint-disable import/extensions */
 import { fetchLoggedInUser, signupAndLoginHandler, setNav } from "./global.js";
 
+const validateCreateForm = (event) => {
+  event.preventDefault();
+
+  // Clear previous error messages
+  const errorMessages = document.querySelectorAll(".help.is-danger");
+  errorMessages.forEach((errorMessage) => {
+    errorMessage.textContent = "";
+  });
+
+  // Validate username and password
+  const usernameInput = document.getElementById("username");
+  const passwordInput = document.getElementById("password");
+  let isValid = true;
+
+  if (usernameInput.value.trim() === "") {
+    isValid = false;
+    document.getElementById("username-error").textContent = "Username is required.";
+  }
+
+  if (passwordInput.value.trim() === "") {
+    isValid = false;
+    document.getElementById("password-error").textContent = "Password is required.";
+  }
+
+  // If form is valid, call the signupAndLoginHandler
+  if (isValid) {
+    signupAndLoginHandler("/api/users", event.target);
+  }
+};
+
 const main = async () => {
   const user = await fetchLoggedInUser();
   if (user) return window.location.assign("/user.html");
@@ -8,10 +38,7 @@ const main = async () => {
   setNav();
   document
     .querySelector("#create-form")
-    .addEventListener("submit", async (event) => {
-      event.preventDefault();
-      signupAndLoginHandler("/api/users", event.target);
-    });
+    .addEventListener("submit", validateCreateForm);
 };
 
 main();
--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,17 +1,44 @@
 /* eslint-disable import/extensions */
 import { fetchLoggedInUser, signupAndLoginHandler, setNav } from "./global.js";
 
+const validateLoginForm = (event) => {
+  event.preventDefault();
+
+  // Clear previous error messages
+  const errorMessages = document.querySelectorAll(".help.is-danger");
+  errorMessages.forEach((errorMessage) => {
+    errorMessage.textContent = "";
+  });
+
+  // Validate username and password
+  const usernameInput = document.getElementById("username");
+  const passwordInput = document.getElementById("password");
+  let isValid = true;
+
+  if (usernameInput.value.trim() === "") {
+    isValid = false;
+    document.getElementById("username-error").textContent = "Username is required.";
+  }
+
+  if (passwordInput.value.trim() === "") {
+    isValid = false;
+    document.getElementById("password-error").textContent = "Password is required.";
+  }
+
+  // If form is valid, call the signupAndLoginHandler
+  if (isValid) {
+    signupAndLoginHandler("/api/users/login", event.target);
+  }
+};
+
 const main = async () => {
   const user = await fetchLoggedInUser();
   if (user) return window.location.assign("/user.html");
 
   setNav();
   document
-    .querySelector("#create-form")
-    .addEventListener("submit", async (event) => {
-      event.preventDefault();
-      signupAndLoginHandler("/api/users/login", event.target);
-    });
+    .querySelector("#login-form")
+    .addEventListener("submit", validateLoginForm);
 };
 
 main();
